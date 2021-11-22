72c618bbc367e44b1914cefd44a26cbae5376f8b
DimitriiTrater <52676168+DimitriiTrater@users.noreply.github.com>
2021-11-22 17:11:15 +0500

    Update (dark_urls)

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from todolist.views import todo
 from todolist.views import category
+from todolist.views import todo_dark, category_dark
 #from todolist.views import alltc
 from todolist.views import redirect_view
 
@@ -9,7 +10,9 @@ from todolist.views import redirect_view
 urlpatterns = [
 	path('', redirect_view),
 	path('todo/', todo, name = "TodoList"),
+	path('todo_dark/', todo_dark),
     #path('alltc/', alltc),
+    path('category_dark/', category_dark),
 	path('category/', category, name = "Category"),
     path('admin/', admin.site.urls),
 ]
